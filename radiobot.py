82fba75123646a255b65db7f0b803b59dca04c65
Roberto Paes <robertocpaes@gmail.com>
2021-12-04 19:19:17 -0300

    fix rádio-boberto

--- radiobot.py
+++ radiobot.py
@@ -16,28 +16,22 @@ load_dotenv()
 
 
 TOKEN = os.getenv("DISCORD_RADIOBOT_TOKEN")         # collected from Discord Bot setup process.
-PREFIX = os.getenv("DISCORD_RADIOBOT_PREFIX")       # e.g. "!"
+PREFIX = os.getenv("DISCORD_RADIOBOT_PREFIX") + ' '      # e.g. "!"
 SOURCE = os.getenv("DISCORD_RADIOBOT_SOURCE")       # e.g. "http://nthmost.net:8000/mutiny-studio"                                  # options: ogg, mp3  (default: ogg)
 
-client = Bot(command_prefix=list(PREFIX))
+client = Bot(command_prefix=PREFIX,description="Eu sou um bot para transmitir músicas por streaming <3")
 
 player = None
 volume_config = 1.0
 
 @client.event
 async def on_ready():
-    print('KSTK Player Ready')
-
-
-@client.command(name="eu")
-async def whoami(ctx) :
-    await ctx.send(f"You are {ctx.message.author.name}")
+    print('Rádio Boberto pronto!')
 
 
 @client.command(name="ouvintes")
 async def web_listeners(ctx):
     await ctx.send(f"Ouvintes conectados em {SOURCE}")
-
 
 async def do_play(ctx):
     global player
@@ -66,7 +60,7 @@ async def do_play(ctx):
 
 
 
-@client.command(aliases=['p', 'pla','toc'])
+@client.command(aliases=['p', 'pla','toc','r','repr'])
 async def play(ctx):
     await do_play(ctx)
 
@@ -93,6 +87,14 @@ async def volume(ctx, *args):
         await ctx.send(f"Volume alterado para {args[0]}")
     else:
         await ctx.send('O volume precisa estar entre 0 e 100')
+
+@client.command(aliases=['sai', 'sair', 'exit'])
+async def sair(ctx, *args):
+    if not ctx.author.voice or not ctx.author.voice.channel:
+        return await ctx.reply('Você não está em um canal de voz.')
+    await ctx.guild.voice_client.disconnect()
+    await ctx.reply('Ok... você me excluiu da fofoca :(')
+        
 
 
 
